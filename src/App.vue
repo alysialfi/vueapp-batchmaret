441c8f2ed3138b8c8a54f4d1a5d065bd633aa435
Alysia Alfi <alfi@domainesia.com>
2023-03-18 14:15:16 +0700

    feat: change app vue

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,45 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo.png">
+    <h1>First Match: {{ firstMatch }}</h1>
+    <h1 :class="secondMatch >= 10 ? 'text-red' : 'text-gray'">Second Match: {{ secondMatch }}</h1>
+    <h1 v-if="secondMatch >= 10">
+        Result: {{ firstMatch + secondMatch }}
+    </h1>
+    <h1 v-else>
+        Second match kurang dari 10
+    </h1>
+
+    <button @click="firstMatch += 1">Tambah first match</button>
+    <br>
+    <br>
+    <button @click="secondMatch += 1">Tambah second match</button>
+    <br>
+    <br>
+
+    <input v-model="scoreFromUser" type="text" name="input-score" id="input-score" placeholder="Masukkan score">
+    <button @click="pushDatatoArray()">Tambahkan data</button>
+    <h1>Perolehan nilai sementara</h1>
+    <p v-for="(score, s) in scores" :key="s">{{ score }}</p>
+    {{ scores }}
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  data() {
+    return {
+        firstMatch: 0,
+        secondMatch: 0,
+        scores: [ 12, 21, 20, 15 ],
+        scoreFromUser: 0,
+    }
+  },
+  methods: {
+    pushDatatoArray() {
+        this.scores.push(parseFloat(this.scoreFromUser));
+    }
+  },
 }
 </script>
 
@@ -22,5 +51,11 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.text-red {
+    color: red;
+}
+.text-gray {
+    color: gray;
 }
 </style>
